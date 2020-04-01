6a2336fe70ef5236dd19f248d8d84dbca23b6189
wrivaswaro <williamrortiz@gmail.com>
2020-04-01 16:54:31 -0700

    version 1

---
+++ app/Controllers/Prueba.php
@@ -0,0 +1,16 @@
+<?php namespace App\Controllers;
+
+class Prueba extends BaseController
+{
+	public function index()
+	{
+		//return view('welcome_message');
+	}
+
+    public function test()
+    {
+        echo "Esto es una prueba";
+    }
+	//--------------------------------------------------------------------
+
+}
